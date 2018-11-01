e3d5fdb1421273729e1de3239658f85916b473f2
Sabbir Ahmed <sabbirs93@gmail.com>
2018-11-01 16:50:11 +0600

    Create style.js

---
+++ text-effect/Effect_01/js/style.js
@@ -0,0 +1,21 @@
+// Wrap every letter in a span
+$('.ml9 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: function(el, i) {
+      return 45 * (i+1)
+    }
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
